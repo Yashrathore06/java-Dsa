e8a7372259801559780a6daa654178343bf95518
yash rathore <yasrathore06@gmail.com>
2026-04-25 00:18:48 +0530

    24 April

---
+++ LoginAttemptLock.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LoginAttemptLock {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String Correctpassword ="yash123";
+        int attempts=0;
+        boolean success=false;
+
+        while (attempts<3){
+            System.out.print("Enter password:");
+            String pass = sc.nextLine();
+
+            if(pass.equals(Correctpassword)){
+                System.out.println("correct password:");
+                success=true;
+                break;
+            }else{
+                System.out.println("wrong");
+                attempts++;
+            }
+
+        }
+        if(!success){
+            System.out.println("Account Locked");
+        }
+    }
+}
